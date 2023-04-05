685cf04168342faa9c8becb5326232753e0f2156
Austin Burke <austinlayneburke@gmail.com>
2023-04-05 07:08:59 -0600

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Austin Burke on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
