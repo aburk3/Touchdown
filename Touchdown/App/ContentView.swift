8b96581efd6e1c02071baf58ea99da6d5d99dd5d
Austin Burke <austinlayneburke@gmail.com>
2023-04-05 07:28:27 -0600

    Create project w initial config and assets

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        FooterView()
+            .padding(.horizontal)
     }
 }
 
