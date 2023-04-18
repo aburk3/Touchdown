c0bef3daf6211b4b621af2896bfb9fcd490842a9
Austin Burke <austinlayneburke@gmail.com>
2023-04-18 06:15:00 -0600

    Add NavigationBarView and LogoView

---
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,51 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Austin Burke on 4/18/23.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            })
+            
+            Spacer()
+            
+            LogoView()
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                    .foregroundColor(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            }) //: BUTTON
+        } //: HSTACK
+    }
+}
+
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
