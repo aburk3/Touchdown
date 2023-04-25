48ac2b0c37994dad36eee96bf09747ca9dccfad4
Austin Burke <austinlayneburke@gmail.com>
2023-04-25 06:39:07 -0600

    Add player model and decoder helper
    Add player model and decoder helper

---
+++ Touchdown/Modal/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Austin Burke on 4/25/23.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
